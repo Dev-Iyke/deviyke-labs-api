f95eefac4f9d3c0448394eb65347edd109fa7589
Dev-Iyke <iyke.thompson01@gmail.com>
2026-08-14 13:47:49 +0100

    Setup projects full seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactSubmissionsModule } from 'src/contact-submissions/contact-submissions.module';
+import { ProjectsModule } from 'src/projects/projects.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ContactSubmissionsModule } from 'src/contact-submissions/contact-submis
       isGlobal: true,
     }),
     ContactSubmissionsModule,
+    ProjectsModule
   ],
   controllers: [AppController], // Controllers are registered here so that they can handle incoming requests and return responses
   providers: [AppService], // Services are registered here so that they can be injected into controllers and other services
